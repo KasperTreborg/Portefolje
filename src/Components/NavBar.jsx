23eec87eba04cf53d76518d2462a46472e1600e2
KasperTreborg <kaspertreborg@outlook.com>
2024-03-03 20:18:51 +0100

    working on scalability (ew)

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,20 @@
 
 export default function NavBar() {
     return (
-        <nav className="sticky top-0 flex flex-wrap items-center justify-between mx-auto p-6 pl-36 pr-36 font-RaleWay text-zinc-200 bg-zinc-900">
+        <nav className="sticky top-0 flex flex-wrap items-center justify-between md:mx-auto p-6 md:px-36 font-RaleWay text-zinc-200 bg-zinc-900">
 
             <div className="flex space-x-2">
                 <span className="text-2xl font-bold font-Playfair">Treborg.</span>
             </div>
 
-            <div className="flex space-x-14 text-xl">
+            <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-zinc-200 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 hover:bg-gray-700" aria-controls="navbar-default" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+
+            <div className="hidden md:flex space-x-14 text-xl">
                 <a href="#top" className="navbar-link">Home</a>
                 <a href="#about" className="navbar-link">About</a>
                 <a href="#contact" className="navbar-link">Contact</a>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react'
 
 import NavBar from '/src/Components/NavBar'
 import ProjectList from '/src/Components/ProjectList'
@@ -9,16 +8,16 @@ export default function Home() {
         <div className="relative text-zinc-200">
             <div id='top' />
             <NavBar />
-            <div id="home" className="h-max">
-                <div id="mainContent" className="pt-40 flex flex-wrap justify-between mx-auto p-6 pl-36 pr-36 font-RaleWay">
+            <div id="home" className="h-screen">
+            <div id="mainContent" className="pt-40 flex flex-col xl:flex-row xl:items-start flex-wrap xl:justify-between mx-auto p-6 md:px-36 xl:px-36 font-RaleWay">
 
                     <div id="nameAndIntro" className="flex flex-col">
-                        <span className="text-8xl pb-12 text-left font-Playfair">I'm Kasper Treborg</span>
-                        <span className="pt-4 text-2xl">Currently getting my bachelors in Informatics-Mathematics-Economics at UiB!</span>
-                        <span className="pt-4 text-2xl">After my degree I would like to work as a frontend- or fullstack developer.</span>
-                        <span className="pt-4 text-2xl pb-10">If you'd like to get in contact with me, click the contact button up top!</span>
+                        <span className="text-4xl md:text-6xl lg:text-7xl 2xl:text-8xl pb-6 text-left font-Playfair">I'm Kasper Treborg</span>
+                        <span className="text-xs p-1 md:pt-4 lg:text-lg 2xl:text-2xl">Currently getting my bachelors in Informatics-Mathematics-Economics at UiB!</span>
+                        <span className="text-xs p-1 md:pt-4 lg:text-lg 2xl:text-2xl">After my degree I would like to work as a frontend- or fullstack developer.</span>
+                        <span className="text-xs p-1 md:pt-4 lg:text-lg 2xl:text-2xl pb-20 xl:pb-10 ">If you'd like to get in contact with me, click the contact button up top!</span>
 
-                        <span className="pt-4 text-2xl">Check out some of my projects on the right &#8594;</span>
+                        <span className="hidden text-sm p-1 md:pt-4 md:text-2xl xl:block xl:text-lg 2xl:text-2xl">Check out some of my projects on the right &#8594;</span>
                     </div>
 
                     <ProjectList />
@@ -27,11 +26,11 @@ export default function Home() {
             </div>
 
             {/* Scroll page: */}
-            <div id="about" className="top-0 h-screen flex flex-col items-center justify-center text-white">
-                <h2 className="text-8xl">ABOUT</h2>
+            <div id="about" className="top-0 h-screen flex flex-col items-center justify-center">
+                <h2 className="text-8xl font-Playfair text-">ABOUT</h2>
             </div>
 
-            <div id="contact" className="top-0 h-screen flex flex-col items-center justify-center text-white">
+            <div id="contact" className="top-0 h-screen flex flex-col items-center justify-center">
                 <h2 className="text-8xl">CONTACT</h2>
             </div>
         </div>
